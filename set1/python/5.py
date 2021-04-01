b5c6b20095ef6a4cfeef9d71f6785512713018f9
aditya-giri <aditya.giridharan@gmail.com>
2021-04-01 16:20:22 +0530

    Formatting changes

--- set1/python/5.py
+++ set1/python/5.py
@@ -24,6 +24,7 @@ def repeating_key_xor(word, key):
         ]
     ).encode().hex()
 
+
 def main():
     plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
     ciphertext = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
@@ -31,5 +32,7 @@ def main():
     print(repeating_key_xor(plaintext, key))
     assert repeating_key_xor(plaintext, key) == ciphertext, "Expected output does not match actual output!"
 
+
 if __name__ == '__main__':
     main()
+
